1a68f7b1d56b4fdb1c222bacdc65cff338016d68
PARK SIWON <zow777@naver.com>
2022-06-09 13:48:55 +0900

    220609 SW: account views update

--- drf_pjt_back/accounts/urls.py
+++ drf_pjt_back/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views1
+from . import views
 
 app_name = 'accounts'
 # 회원가입(POST) : account/signup/
@@ -8,10 +8,10 @@ app_name = 'accounts'
 # 비밀번호 찾기(POST) : account/password/reset/
 
 urlpatterns = [
-    path('<int:user_pk>/profile/', views1.profile), # 프로필 조회 및 수정
+    path('<int:user_pk>/profile/', views.profile), # 프로필 조회 및 수정
     # path('signout/', views.signout), # 회원탈퇴
-    path('<int:user_pk>/roadmap/', views1.user_roadmap), # 사용자 로드맵 조회 및 수정
-    path('<int:user_pk>/roadmap/detail/', views1.user_roadmap_detail), # 사용자 로드맵 상세 조회(이행 수준)
-    path('<int:user_pk>/bookmark/', views1.bookmark), # 북마크 조회 및 등록 및 해제
-    path('partners/', views1.partners), # 개발 동료 찾기
+    path('<int:user_pk>/roadmap/', views.user_roadmap), # 사용자 로드맵 조회 및 수정
+    path('<int:user_pk>/roadmap/detail/', views.user_roadmap_detail), # 사용자 로드맵 상세 조회(이행 수준)
+    path('<int:user_pk>/bookmark/', views.bookmark), # 북마크 조회 및 등록 및 해제
+    path('partners/', views.partners), # 개발 동료 찾기
 ]
