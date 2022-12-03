f982e21248bfcfb330758c9047420d496668facf
siwon-park <zow777@naver.com>
2022-12-04 00:04:24 +0900

    initail commit

--- drf_pjt_back/articles/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from articles.models import Article, Comment
-
-
-# Register your models here.
-admin.site.register(Article)
-admin.site.register(Comment)
--- drf_pjt_back/skills/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from skills.models import Knowledge, Type, Skill
-
-admin.site.register(Type)
-admin.site.register(Skill)
-admin.site.register(Knowledge)
-
